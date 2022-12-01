6a5a65b690ccf0c8b2d1ff3d071b29fa63acf599
David Gray <davidgraymi@gmail.com>
2022-11-30 23:10:54 -0600

    removed git scripts from repo

--- build/branch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Creates branch locally and remotely, and checks it out
-
-git branch $1
-git push --set-upstream origin $1
-git checkout $1
--- build/delete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Deletes local, remote, and reference branch
-
-git push origin -d $1
-git branch -d $1
